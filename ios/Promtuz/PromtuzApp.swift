9e8d6a5c40986bbb56e10a1e1266812afef766f2
Bhuvnesh <notbhuvnesh@gmail.com>
2026-07-03 01:48:36 +0530

    init(ios): initialize empty iOS project

---
+++ ios/Promtuz/PromtuzApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromtuzApp.swift
+//  Promtuz
+//
+//  Created by Bhuvnesh Kumar on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PromtuzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
